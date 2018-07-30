933ad0c2b5af1c63c320ff0a339f0efb29e78cf1
Jossay Jacobo <jossjacobo@gmail.com>
2018-07-30 09:39:47 -0400

    Changed to a daily run for list

--- main.test.js
+++ main.test.js
@@ -16,72 +16,84 @@ fixture('Whisk(e)y Hunting!')
   .page('https://distiller.com/')
 
 test('Sign In to distiller and print wishlist', async t => {
-  const whiskeyList = await new DistillerHomePage()
-    .navigateToLoginPage()
-    .then(signInPage => signInPage.signIn())
-    .then(homePage => homePage.navigateToProfile())
-    .then(profilePage => profilePage.navigateToMyLists())
-    .then(myListsPage => myListsPage.navigateToList())
-    .then(wishList => wishList.getWhiskeyNames())
-    .catch(err => console.log(err));
+  try {
+    const whiskeyList = await new DistillerHomePage()
+      .navigateToLoginPage()
+      .then(signInPage => signInPage.signIn())
+      .then(homePage => homePage.navigateToProfile())
+      .then(profilePage => profilePage.navigateToMyLists())
+      .then(myListsPage => myListsPage.navigateToList())
+      .then(wishList => wishList.getWhiskeyNames())
+      .catch(err => console.log(err));
 
-  console.log(whiskeyList);
-  if (process.env.SLACK_WEBHOOK_URL) {
-    slack.send({
-      icon_url: 'https://slack-files2.s3-us-west-2.amazonaws.com/avatars/2018-07-29/408349589526_501be22227b357239684_72.jpg',
-      unfurl_links: 1,
-      username: 'Whiskey Bot',
-      text: `Searching Whiskey Distiller List '${process.env.DISTILLER_LIST}': \n \`\`\`${JSON.stringify(whiskeyList, null, 2)}\`\`\``
-    });
-  }
-
-  if (whiskeyList.length > 0) {
-    await t.navigateTo('https://www.abc.virginia.gov/');
-    let results = [];
-    for (const whiskey of whiskeyList) {
-      await new ABCHomePage().search(whiskey);
-      const resultsPage = new ABCSearchResultsPage();
-      if (resultsPage.hasResults()) {
-        await resultsPage.clickOnFirstResult()
-          .then(productPage => productPage.selectSizeIfAvailable())
-          .then(productPage => productPage.searchAllStoresInventory(whiskey))
-          .then(res => results.push(res))
-          .catch(err => console.log(err));
-      }
-    }
-
-    // Reduce the results array to sort them by store and not by whiskey
-    const resultsSortedByStores = results.reduce((sorted, whiskey) => {
-      for (const store of whiskey.results) {
-        sorted[`${store.storeInfo.storeId}`] = [];
-        sorted[store.storeInfo.storeId].push({
-          searched: whiskey.searchedWhiskey,
-          found: whiskey.foundWhiskey,
-          inventory: store.results.inventoryCount,
-          phone: store.results.phone
-          // address: store.storeInfo.address // TODO clean up address field
-        });
-      }
-      return sorted;
-    }, {});
-
-    console.log(util.inspect(resultsSortedByStores, false, null));
+    console.log(whiskeyList);
     if (process.env.SLACK_WEBHOOK_URL) {
       slack.send({
         icon_url: 'https://slack-files2.s3-us-west-2.amazonaws.com/avatars/2018-07-29/408349589526_501be22227b357239684_72.jpg',
         unfurl_links: 1,
         username: 'Whiskey Bot',
-        text: `Search results:\n\`\`\`${JSON.stringify(resultsSortedByStores, null, 2)}\`\`\``
+        text: `Searching for Distiller Whiskey List *${process.env.DISTILLER_LIST}*: \n \`\`\`${JSON.stringify(whiskeyList, null, 2)}\`\`\``
       });
     }
-  } else {
-    console.log('No whisk(e)ys found on your list');
+
+    if (whiskeyList.length > 0) {
+      await t.navigateTo('https://www.abc.virginia.gov/');
+      let results = [];
+      for (const whiskey of whiskeyList) {
+        await new ABCHomePage().search(whiskey);
+        const resultsPage = new ABCSearchResultsPage();
+        if (resultsPage.hasResults()) {
+          await resultsPage.clickOnFirstResult()
+            .then(productPage => productPage.selectSizeIfAvailable())
+            .then(productPage => productPage.searchAllStoresInventory(whiskey))
+            .then(res => results.push(res))
+            .catch(err => console.log(err));
+        }
+      }
+
+      // Reduce the results array to sort them by store and not by whiskey
+      const resultsSortedByStores = results.reduce((sorted, whiskey) => {
+        for (const store of whiskey.results) {
+          sorted[`${store.storeInfo.storeId}`] = [];
+          sorted[store.storeInfo.storeId].push({
+            searched: whiskey.searchedWhiskey,
+            found: whiskey.foundWhiskey,
+            inventory: store.results.inventoryCount,
+            phone: store.results.phone
+            // address: store.storeInfo.address // TODO clean up address field
+          });
+        }
+        return sorted;
+      }, {});
+
+      console.log(util.inspect(resultsSortedByStores, false, null));
+      if (process.env.SLACK_WEBHOOK_URL) {
+        slack.send({
+          icon_url: 'https://slack-files2.s3-us-west-2.amazonaws.com/avatars/2018-07-29/408349589526_501be22227b357239684_72.jpg',
+          unfurl_links: 1,
+          username: 'Whiskey Bot',
+          text: `Search results:\n\`\`\`${JSON.stringify(resultsSortedByStores, null, 2)}\`\`\``
+        });
+      }
+    } else {
+      console.log('No whisk(e)ys found on your list');
+      if (process.env.SLACK_WEBHOOK_URL) {
+        slack.send({
+          icon_url: 'https://slack-files2.s3-us-west-2.amazonaws.com/avatars/2018-07-29/408349589526_501be22227b357239684_72.jpg',
+          unfurl_links: 1,
+          username: 'Whiskey Bot',
+          text: 'No whiskeys found on your list'
+        });
+      }
+    }
+  } catch (error) {
+    console.log(error);
     if (process.env.SLACK_WEBHOOK_URL) {
       slack.send({
         icon_url: 'https://slack-files2.s3-us-west-2.amazonaws.com/avatars/2018-07-29/408349589526_501be22227b357239684_72.jpg',
         unfurl_links: 1,
         username: 'Whiskey Bot',
-        text: 'No whiskeys found on your list'
+        text: error
       });
     }
   }
